cb9f2356dac2eba618bec6dc000692e0a04930c1
Amna Shah <amnashah4221@gmail.com>
2026-07-17 19:39:06 +0500

    Fix import path for JobDescText component

--- frontend/src/pages/AnalyzerPage.jsx
+++ frontend/src/pages/AnalyzerPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import ResumeDropzone from '../components/ResumeDropzone.jsx';
-import JobDescText from '../components/JobDescText.jsx';
+import JobDescText from '../components/JobDesText.jsx';
 import ToneSelector from '../components/ToneSelector.jsx';
 import MatchScore from '../components/MatchScore.jsx';
 import SkillFeedback from '../components/SkillFeedback.jsx';
