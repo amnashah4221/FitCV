c16cd9376d356f2cc2fd686d02f1a585667be051
amnashah4221 <amnashah4221@gmail.com>
2026-07-17 16:29:49 +0500

    Deploy settings

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 })
 
 api.interceptors.request.use((config) => {
@@ -11,7 +11,6 @@ api.interceptors.request.use((config) => {
   }
   return config
 })
-
 
 api.interceptors.response.use(
   (response) => response,
