b4865a712b9e8b90121f8b10a22de4c6a0ac493d
amnashah4221 <amnashah4221@gmail.com>
2026-07-17 15:15:49 +0500

    Deploy settings

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,11 @@ app.get('/', (req, res) => {
   res.json({ message: 'FitCV API running ✓' })
 })
 
+module.exports = app;
+
+if(require.main === module) {
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+}
